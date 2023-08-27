cb5a143b2316e94d0249cf4306e2306b8ad6142b
ronitchawla <ronit.chawla2008@gmail.com>
2023-08-27 20:47:27 +0530

    added device check

--- App.js
+++ App.js
@@ -1,24 +1,34 @@
 import MainContainer from './navigation/MainContainer';
-import { View, Text } from 'react-native';
-import AppLoading from 'expo-app-loading';
+import { View } from 'react-native';
 import { useFonts } from 'expo-font';
-import { useState } from 'react';
-import { setCustomText } from 'react-native-global-props';
+import * as Device from 'expo-device';
 
 export default function App() {
+	console.log('type:' + Device.deviceType);
 	const [
 		fontLoaded
 	] = useFonts({
 		EncodeSans : require('./assets/fonts/EncodeSans_Expanded-Regular.ttf')
 	});
+	// If Device.deviceType === 0 -> The app is running on an UNKNOWN DEVICE
+	// Our Device being a spy tool is unknown and therefore using this we can check whether the device is authorised
+	// For now we'll check if Device.deviceType === 1 -> The app is running on a PHONE
 	return (
 		<View
 			style={{
-				fontFamily : 'EncodeSans',
-				flex       : 1
+				flex : 1
 			}}
 		>
-			<MainContainer />
+			{Device.deviceType === 1 ? (
+				<MainContainer />
+			) : (
+				<View
+					style={{
+						backgroundColor : '#000',
+						flex            : 1
+					}}
+				/>
+			)}
 		</View>
 	);
 }
